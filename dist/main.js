728abffbbefc4528f3c28c4ded2e035399b966a7
Rishi Raj <rishiraj007bgp@gmail.com>
2023-11-13 19:33:10 +0530

    setup project with webpack

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("\n\nconst h1 = document.createElement('h1')\nh1.textContent = 'Hello'\ndocument.body.appendChild(h1);\n\n// This is reference to access data in code:\n// {\n//     \"location\": {\n//       \"name\": \"London\",\n//       \"region\": \"City of London, Greater London\",\n//       \"country\": \"United Kingdom\",\n//       \"lat\": 51.52,\n//       \"lon\": -0.11,\n//       \"tz_id\": \"Europe/London\",\n//       \"localtime_epoch\": 1699876642,\n//       \"localtime\": \"2023-11-13 11:57\"\n//     },\n//     \"current\": {\n//       \"last_updated_epoch\": 1699875900,\n//       \"last_updated\": \"2023-11-13 11:45\",\n//       \"temp_c\": 15,\n//       \"temp_f\": 59,\n//       \"is_day\": 1,\n//       \"condition\": {\n//         \"text\": \"Partly cloudy\",\n//         \"icon\": \"//cdn.weatherapi.com/weather/64x64/day/116.png\",\n//         \"code\": 1003\n//       },\n//       \"wind_mph\": 27.3,\n//       \"wind_kph\": 43.9,\n//       \"wind_degree\": 250,\n//       \"wind_dir\": \"WSW\",\n//       \"pressure_mb\": 995,\n//       \"pressure_in\": 29.38,\n//       \"precip_mm\": 0,\n//       \"precip_in\": 0,\n//       \"humidity\": 59,\n//       \"cloud\": 25,\n//       \"feelslike_c\": 13.2,\n//       \"feelslike_f\": 55.8,\n//       \"vis_km\": 10,\n//       \"vis_miles\": 6,\n//       \"uv\": 4,\n//       \"gust_mph\": 24.6,\n//       \"gust_kph\": 39.6\n//     }\n//   }\n\n//# sourceURL=webpack://weather_app/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,48 @@
+
+
+const h1 = document.createElement('h1')
+h1.textContent = 'Hello'
+document.body.appendChild(h1);
+
+// This is reference to access data in code:
+// {
+//     "location": {
+//       "name": "London",
+//       "region": "City of London, Greater London",
+//       "country": "United Kingdom",
+//       "lat": 51.52,
+//       "lon": -0.11,
+//       "tz_id": "Europe/London",
+//       "localtime_epoch": 1699876642,
+//       "localtime": "2023-11-13 11:57"
+//     },
+//     "current": {
+//       "last_updated_epoch": 1699875900,
+//       "last_updated": "2023-11-13 11:45",
+//       "temp_c": 15,
+//       "temp_f": 59,
+//       "is_day": 1,
+//       "condition": {
+//         "text": "Partly cloudy",
+//         "icon": "//cdn.weatherapi.com/weather/64x64/day/116.png",
+//         "code": 1003
+//       },
+//       "wind_mph": 27.3,
+//       "wind_kph": 43.9,
+//       "wind_degree": 250,
+//       "wind_dir": "WSW",
+//       "pressure_mb": 995,
+//       "pressure_in": 29.38,
+//       "precip_mm": 0,
+//       "precip_in": 0,
+//       "humidity": 59,
+//       "cloud": 25,
+//       "feelslike_c": 13.2,
+//       "feelslike_f": 55.8,
+//       "vis_km": 10,
+//       "vis_miles": 6,
+//       "uv": 4,
+//       "gust_mph": 24.6,
+//       "gust_kph": 39.6
+//     }
+//   }
